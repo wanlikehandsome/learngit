e685a66cc61ce2ae500d5728e4abf51c9fcae4f0
txb <2563418641@qq.com>
2019-04-16 22:52:12 +0800

    数据库管理系系统

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String [] args) {
+        String i = "txb";
+        if("txb".equals(i)) {
+            System.out.println("you are right!");
+        }
+    }
+}
